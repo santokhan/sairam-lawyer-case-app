7601c30dfbc1682f2e054f216ace140928982110
Santo <santokhan1999@gmail.com>
2023-09-25 13:54:32 +0600

    Initial

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+//Import it from your react index.js file
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+)
